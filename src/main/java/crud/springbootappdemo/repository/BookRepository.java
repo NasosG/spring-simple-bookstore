6589142902f55fe737c5c2de39185db798d6545f
Thanasis Galatis <gnasos219@gmail.com>
2021-03-04 12:10:22 +0200

    [master] - made the query better

--- src/main/java/crud/springbootappdemo/repository/BookRepository.java
+++ src/main/java/crud/springbootappdemo/repository/BookRepository.java
@@ -13,7 +13,8 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByTitleContaining(String title);
 
-    @Query(value="SELECT * FROM BOOK INNER JOIN AUTHOR au ON au.author_id = BOOK.author_id WHERE LOWER(au.firstname) LIKE CONCAT('%',LOWER(:name),'%') OR LOWER(au.lastname) LIKE CONCAT('%',LOWER(:name),'%') ", nativeQuery = true)
+    //@Query(value="SELECT * FROM BOOK INNER JOIN AUTHOR au ON au.author_id = BOOK.author_id WHERE LOWER(au.firstname) LIKE CONCAT('%',LOWER(:name),'%') OR LOWER(au.lastname) LIKE CONCAT('%',LOWER(:name),'%') ", nativeQuery = true)
+    @Query(value="SELECT * FROM BOOK INNER JOIN AUTHOR a ON a.author_id = BOOK.author_id WHERE CONCAT(LOWER(a.firstname), ' ' ,LOWER(a.lastname)) LIKE CONCAT('%',LOWER(:name),'%') ", nativeQuery = true)
     List<Book> findByAuthorContaining(@Param("name") String name);
 
     @Query(value="SELECT TOP 1 * FROM BOOK ORDER BY PUBLICATION_YEAR DESC", nativeQuery = true)
